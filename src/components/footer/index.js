fa2659c28eb126a254efe163bbcb58abb245b7f1
Hamza <arastiranhamza@gmail.com>
2023-07-24 23:11:05 +0300

    added responsive in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,9 +3,12 @@ import "./Styles.Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <img src={require("../../assets/images/footericon.png")} />
-      <span>
+    <div className="footer-container">
+      <img
+        className="footer-icon"
+        src={require("../../assets/images/footericon.png")}
+      />
+      <span className="footer-title">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
         malesuada dapibus ut pulvinar neque arcu, commodo. Pharetra nisi egestas
         nisi fermentum. Sollicitudin egestas senectus pellentesque enim mauris
