74aa773373d5a8c367686de936d43bb116f005b4
Hamza <arastiranhamza@gmail.com>
2023-07-24 22:44:46 +0300

    added image and text

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Styles.Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <img src={require("../../assets/images/footericon.png")} />
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit
+        malesuada dapibus ut pulvinar neque arcu, commodo. Pharetra nisi egestas
+        nisi fermentum. Sollicitudin egestas senectus pellentesque enim mauris
+        vel odio commodo. Pellentesque orci vestibulum sed in molestie
+        consequat.
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
