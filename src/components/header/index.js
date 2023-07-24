284c30448640096349628b00425a0baf14b1cacc
Hamza <arastiranhamza@gmail.com>
2023-07-24 21:25:50 +0300

    added header-icon with login button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./Styles.Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-container">
+      <img src={require("../../assets/images/headericon.png")} />
+      <div className="header-right-container">
+        <div className="header-button">
+          <img src={require("../../assets/images/avatar.png")} />
+          <span className="header-button-title">Giriş yap</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
