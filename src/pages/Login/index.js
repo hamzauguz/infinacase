27ec171cdd23daeab05cd5b2c14dfd0a3698e9a4
Hamza <arastiranhamza@gmail.com>
2023-07-25 08:25:06 +0300

    added login image responsive

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,7 +4,10 @@ import "./Styles.Login.css";
 const Login = () => {
   return (
     <div className="login-container">
-      <img src={require("../../assets/images/mobile-login.png")} />
+      <img
+        className="login-image"
+        src={require("../../assets/images/mobile-login.png")}
+      />
       <div className="login-right-container">
         <span className="login-right-title">
           En uygun fiyatlara ulaşmak <br /> için giriş yapın!
