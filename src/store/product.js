525954e88f1d31ef8c0099e4dffc158f65ec77c0
Hamza <arastiranhamza@gmail.com>
2023-07-26 01:14:55 +0300

    added fetchProduct, getData at Products

--- src/store/product.js
+++ src/store/product.js
@@ -2,3 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 import { collection, getDocs } from "firebase/firestore";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { db } from "../firebase";
+
+export const fetchProducts = createAsyncThunk(
+  "books/fetchProducts",
+  async () => {
+    const querySnapshot = await getDocs(collection(db, "Products"));
+    const products = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      product: doc.data(),
+    }));
+    return products;
+  }
+);
