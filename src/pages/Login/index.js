93a0b1836357b50a8d381b0dfac26773f114b7d3
Hamza <arastiranhamza@gmail.com>
2023-07-25 06:57:51 +0300

    created right container for login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,6 +5,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <img src={require("../../assets/images/mobile-login.png")} />
+      <div className="login-right-container"></div>
     </div>
   );
 };
