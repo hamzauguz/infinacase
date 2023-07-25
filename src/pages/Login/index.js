c7a15ddc9d9e6a331c5fc94f54e5f1fe95c14c71
Hamza <arastiranhamza@gmail.com>
2023-07-25 07:09:58 +0300

    added login info title in right place

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,7 +5,11 @@ const Login = () => {
   return (
     <div className="login-container">
       <img src={require("../../assets/images/mobile-login.png")} />
-      <div className="login-right-container"></div>
+      <div className="login-right-container">
+        <span className="login-right-title">
+          En uygun fiyatlara ulaşmak <br /> için giriş yapın!
+        </span>
+      </div>
     </div>
   );
 };
