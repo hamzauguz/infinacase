a27a60b948e495f3da9a846348d7af161c527526
Hamza <arastiranhamza@gmail.com>
2023-07-25 10:49:36 +0300

    added labelTitle, value, onChange and type props

--- src/components/label-with-input/index.js
+++ src/components/label-with-input/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Styles.LabelWithInput.css";
 
-const LabelWithInput = () => {
-  return <div>LabelWithInput</div>;
+const LabelWithInput = ({ labelTitle, value, onChange, type }) => {
+  return (
+    <div className="label-with-input-container">
+      <span className="label-lwic">{labelTitle}</span>
+      <input
+        type={type}
+        className="input-style"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
 };
 
 export default LabelWithInput;
