543c77ab5b614a1a29d13980a7fba5c081d9fdc1
Hamza <arastiranhamza@gmail.com>
2023-07-25 11:01:18 +0300

    add dynamic email and password input values

--- src/components/label-with-input/index.js
+++ src/components/label-with-input/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Styles.LabelWithInput.css";
 
-const LabelWithInput = ({ labelTitle, value, onChange, type }) => {
+const LabelWithInput = ({ labelTitle, value, onChange, type, name }) => {
   return (
     <div className="label-with-input-container">
       <span className="label-lwic">{labelTitle}</span>
       <input
+        name={name}
         type={type}
         className="input-style"
         value={value}
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
 import "./Styles.Login.css";
@@ -6,6 +6,21 @@ import LabelWithInput from "../../components/label-with-input";
 
 const Login = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  console.log("email: ", formData.email, formData.password);
 
   return (
     <div className="login-container">
@@ -21,8 +36,20 @@ const Login = () => {
           En uygun fiyatlara ulaşmak <br /> için giriş yapın!
         </span>
         <div className="login-input-container">
-          <LabelWithInput labelTitle={"E-Mailiniz"} type={"text"} />
-          <LabelWithInput labelTitle={"Şifreniz"} type={"password"} />
+          <LabelWithInput
+            name={"email"}
+            value={formData.email}
+            onChange={handleInputChange}
+            labelTitle={"E-Mailiniz"}
+            type={"text"}
+          />
+          <LabelWithInput
+            name={"password"}
+            value={formData.password}
+            onChange={handleInputChange}
+            labelTitle={"Şifreniz"}
+            type={"password"}
+          />
         </div>
         <span className="login-button">Giriş Yap</span>
       </div>
