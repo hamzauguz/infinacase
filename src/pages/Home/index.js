18026c5766d7f6985c387e656f610c2570fe6923
Hamza <arastiranhamza@gmail.com>
2023-07-26 00:36:08 +0300

    added login and entegrated useselector in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./Styles.Home.css";
+import { useSelector } from "react-redux";
+import { logout } from "../../helpers/firebaseAuth";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/login", {
+      replace: true,
+    });
+  };
+
+  console.log("user::", user);
+  return (
+    <div>
+      home <br />
+      {user && (
+        <>
+          <span>giriş yaptın</span>
+          <button onClick={handleLogout}>çıkış yap</button>
+          <h1>hoşgeldin {user.fullName}</h1>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
