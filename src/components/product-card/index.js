fad4801bd124a6668018283334b4fc8f0dd131ee
Hamza <arastiranhamza@gmail.com>
2023-07-26 13:11:16 +0300

    added props, action mouse and count addtobasket

--- src/components/product-card/index.js
+++ src/components/product-card/index.js
@@ -4,8 +4,33 @@ import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 import "./Styles.ProductCard.css";
 
-const ProductCard = () => {
-  return <div></div>;
+const ProductCard = ({
+  productTitle,
+  productPrice,
+  productQuantity,
+  count,
+  setCount,
+}) => {
+  // const [count, setCount] = useState(0);
+
+  const [basketIcon, setBasketIcon] = useState(false);
+
+  const handleMouseEnter = () => {
+    setBasketIcon(true);
+  };
+
+  const handleMouseLeave = () => {
+    setBasketIcon(false);
+  };
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return <div>product card</div>;
 };
 
 export default ProductCard;
