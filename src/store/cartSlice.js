ff489f0d98eae25a4074efaf6e068b9d92a475ab
Hamza <arastiranhamza@gmail.com>
2023-07-26 23:27:23 +0300

    create cartSlice and added addToCart

---
+++ src/store/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  card: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, { payload }) {
+      const { id } = payload;
+
+      const findIndex = state.card.findIndex((item) => item.id === id);
+      if (findIndex !== -1) {
+        state.card[findIndex].quantity += 1;
+      } else {
+        state.card.push({
+          ...payload,
+          quantity: 1,
+        });
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+const cartReducer = cartSlice.reducer;
+
+export default cartReducer;
